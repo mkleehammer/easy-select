14d28f7eef94be53bd058310b96d2977654dc83b
Michael Kleehammer <michael@kleehammer.com>
2024-07-08 23:22:41 -0500

    WIP: Back to minor mode.  Key works

--- select-mode.el
+++ select-mode.el
@@ -16,10 +16,15 @@
 ;;    - Support for direction and expanding in the opposite direction.
 ;;    - Easy setup and tear down for customization
 ;;    - Hooks
-;;
-;; Consider using the types from thing-at-pt.
-(defvar hydra-select-type nil)
-(defvar hydra-select-dir 'forward)
+
+(defvar select-mode--type nil)
+(defvar select-mode--dir 'forward)
+
+(defvar select-mode-map
+  (let ((map (make-sparse-keymap)))
+    (keymap-set map "q" #'select-mode-abort)
+    (keymap-set map "RET" #'select-mode-abort)
+    map))
 
 (defvar select-mode--origin nil
   "An overlay visually marking the original location of point.
@@ -31,17 +36,17 @@ All initial selections of \"things\" start from this point.")
   :group 'killing)
 
 
-(defun select-mode-init ()
+(defun select-mode--setup ()
   "Setup code called when entering the select hydra."
-  (message "init")
+  (message "setup")
 
-  (setq hydra-select-type 'sexp)
-  (setq hydra-select-dir  'forward)
+  (setq select-mode--type 'sexp)
+  (setq select-mode--dir  'forward)
 
   (setq select-mode--origin (make-overlay (point) (1+ (point))))
   (overlay-put select-mode--origin 'face 'select-mode-origin)
 
-  (message "init2: %s" select-mode--origin)
+  (message "setup2: %s" select-mode--origin)
 
   ;; Eventually I'd like either a variable for the initial type or a function
   ;; run to perform the initial select.
@@ -52,22 +57,16 @@ All initial selections of \"things\" start from this point.")
       (goto-char (cdr bounds))))
 
 
-(defun select-mode-cleanup ()
-  "Cleaup code called when exiting select-mode.
+(defun select-mode-exit ()
+  "Exits select mode, leaving the current selection.
 
-This does not change the selection."
-  (message "cleanup")
+This is safe to call it select-mode is not active."
+  (interactive)
+  (message "exit")
   (and (overlayp select-mode--origin)
        (delete-overlay select-mode--origin))
   (setq select-mode--origin nil))
 
-
-(defun select-mode-copy-or-mark ()
-  "Copy if region exists, otherwise start the select hydra."
-  (interactive)
-  (if (use-region-p)
-      (kill-ring-save (region-beginning) (region-end) t)
-    (hydra-select/body)))
 
 (defun select-mode-abort ()
   "Turn off select-mode and remove the selection."
@@ -80,73 +79,27 @@ This does not change the selection."
 ;; (keymap-set global-map "C-M-w" 'select-mode-copy-or-mark)
 
 
-;; TODO: How do we keep multiple instances of this hydra from conflicting?  Is
-;; there something we can attach a plist to?
+(define-minor-mode select-mode
+  "Provides quick commands for selecting and manipulating the region."
+  :lighter "sel"
+  :keymap select-mode-map
+
+  (if select-mode
+      (select-mode--setup))
+  (select-mode-exit))
 
 
-(defhydra hydra-select (:body-pre select-mode-init
-                        :after-exit select-mode-cleanup
-                        :hint nil
-                        :color red)
-  "
-Select
-^Types^          ^Pairs^^^^                ^Find^           ^Misc^
-_w_ word         _[_ ^outer  _]_ inner     _f_ find         _c_ change dir
-_s_ symbol       _(_ ^outer  _)_ inner     _t_ til          _;_ repeat
-_x_ expression   _{_ ^outer  _}_ inner     _F_ find back    _z_ undo
-_l_ line         _\"_ inner  ^ ^           _T_ til back     _-_ shrink
-_._ sentence     _'_ ^inner  _I_ insert    ^ ^
-_p_ paragraph    _`_ ^inner  _C_ change    ^ ^
-_d_ function     ^ ^ ^       _D_ delete         _RET_ exit  _q_ abort
-"
-  ("RET" nil nil :color blue)
-
-  ;; TODO: Can use use the lowercase i c d?  Change shrink to something else -?
-  ;; Maybe change " to inner and hit again for outer?
-  ;; Also conflicts with d for function
-
-  ;; TODO: Need direction key.
-  ;; Any other symbols on base?  , . / ; '
-
-  ;; Could use + for repeat and also make it work on pairs to continue "up".
-
-  ;; TODO: items below this are incomplete
-  (";" hydra-select--word nil)
-  ("z" hydra-select--word nil)
-  ("w" hydra-select--word nil)
-  ("s" hydra-select--WORD nil)
-  ("x" hydra-select--WORD nil)
-  ("l" hydra-select--WORD nil)
-  ("s" hydra-select--WORD nil)
-  ("p" hydra-select--WORD nil)
-  ("." hydra-select--WORD nil)
-  ("(" hydra-select--WORD nil)
-  (")" hydra-select--WORD nil)
-  ("{" hydra-select--WORD nil)
-  ("}" hydra-select--WORD nil)
-  ("[" hydra-select--WORD nil)
-  ("]" hydra-select--WORD nil)
-  ("\"" hydra-select--WORD nil)
-  ("'" hydra-select--WORD nil)
-  ("`" hydra-select--WORD nil)
-  ("f" hydra-select--WORD nil)
-  ("F" hydra-select--WORD nil)
-  ("t" hydra-select--WORD nil)
-  ("T" hydra-select--WORD nil)
-  ("d" hydra-select--WORD nil)
-  ("" hydra-select--WORD nil)
-  ("-" hydra--select-word nil)
-  ("I" hydra--select-word nil :color blue)
-  ("C" hydra--select-word nil :color blue)
-  ("D" hydra--select-word nil :color blue)
-  ("q" select-mode-abort nil :color blue)
-  ("g" nil nil :color blue)
-  )
-
-
-
-(defun hydra-select--word ()
-  )
-
-(defun hydra-select--WORD ()
-  )
+(defun select-mode-copy-or-mark ()
+  "Copy if region exists, otherwise start the select hydra."
+  (interactive)
+  ;; If there is a selection, copy it.  If the mode wasn't on, then it is a
+  ;; normal copy.  If it was on, turn it off and turn off the region.
+  (if (use-region-p)
+      ;; The region is set, so we're going to copy whether the mode was on or
+      ;; not.  If it was on, though, we want to exit and keep the current
+      ;; region, but deactivate the mark.  kill-ring-save will deactivate for
+      ;; us.
+      (progn
+        (kill-ring-save (region-beginning) (region-end) t)
+        (select-mode-exit))             ; safe to call when not on
+    (select-mode)))
