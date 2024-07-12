a40b079726e67e56b46a9a5ad80aa7b5c2a8eb97
Michael Kleehammer <michael@kleehammer.com>
2024-07-12 01:31:24 -0500

    Pairs work (not quotes).
    Quotes aren't selecting upwards.

--- select-mode.el
+++ select-mode.el
@@ -20,6 +20,8 @@ If nil, do not show numbers unless toggled on using \"n\".")
 If nil, do not hide numbers unless toggled off using \"n\".")
 
 
+(defvar select-mode-initial-type 'sexp)
+
 (defvar select-mode--types
   '((line . (
              :key   "l"
@@ -41,22 +43,49 @@ If nil, do not hide numbers unless toggled off using \"n\".")
              :key   "x"
              :start select-mode--sexp-start
              :next  select-mode--sexp-next
+             ))
+    (pair . (
+             :start select-mode--pair-start
+             :next  select-mode--pair-next
              ))))
 
-(defvar select-mode-map (select-mode--make-keymap))
+
+(defvar select-mode-pairs
+  '(("("  . ")")
+    ("{"  . "}")
+    ("["  . "]")
+    ("<"  . ">")
+    ("'"  . "'")
+    ("`"  . "`")
+    ("\"" . "\"")))
+
 
 
 (defun select-mode--make-keymap ()
 
   (let ((map (make-sparse-keymap)))
 
-    ;; Add a key for each type.
+    ;; Add a key for each type except pair (which doesn't have `key' so if-let).
     (dolist (pair select-mode--types)
-      (let ((type (car pair))
-            (key  (plist-get (cdr pair) :key)))
-        (keymap-set map key (lambda ()
-                              (interactive)
-                              (select-mode--set-type type)))))
+      (if-let ((type (car pair))
+               (key  (plist-get (cdr pair) :key)))
+          (keymap-set map key (lambda ()
+                                (interactive)
+                                (select-mode--set-type type)))))
+
+    ;; Add a key for each pair, both open and close if different.
+    (dolist (pair select-mode-pairs)
+      (let* ((left  (car pair))
+             (right (cdr pair)))
+        (define-key map left
+                    (lambda ()
+                      (interactive)
+                      (select-mode--set-type 'pair left)))
+        (unless (string= left right)
+          (define-key map right
+                    (lambda ()
+                      (interactive)
+                      (select-mode--set-type 'pair right))))))
 
     ;; Add keys 1-9 for expanding the selection.
     (dotimes (c 9)
@@ -71,6 +100,13 @@ If nil, do not hide numbers unless toggled off using \"n\".")
 
     map))
 
+(defvar select-mode-map (select-mode--make-keymap))
+
+
+;;
+;; "Session" variables that are used for reset when the mode is enabled
+;; and some when the type is changed.
+;;
 
 (defvar select-mode--origin nil
   "An overlay visually marking the original location of point.
@@ -78,6 +114,15 @@ If nil, do not hide numbers unless toggled off using \"n\".")
 All initial selections of \"things\" start from this point.")
 
 (defvar select-mode--type nil)
+
+(defvar select-mode--key nil
+  "The key pressed, as a string, to enable the current type.
+
+For non-pairs, this will match the type's :key property and isn't
+very useful.  For pairs, it allows code to determine if an open
+or close character was pressed to differentiate between inclusive
+and exclusive (outer and inner) selections.")
+
 
 (defvar select-mode--dir 'forward)
 
@@ -113,6 +158,7 @@ on or off, this is set to the properties always or never.")
   (cl-assert (null select-mode--overlays) "Overlays weren't deleted?")
 
   (setq select-mode--type nil)
+  (setq select-mode--key nil)
   (setq select-mode--dir  'forward)
   (setq select-mode--undo-list nil)
   (setq select-mode--next nil)
@@ -131,7 +177,9 @@ on or off, this is set to the properties always or never.")
               select-mode-map)
 
   (add-hook 'pre-command-hook #'select-mode--hook-func)
-  (select-mode--set-type 'word))
+  (select-mode--set-type select-mode-initial-type)
+  ;;  (select-mode--set-type 'pair ")")
+  )
 
 
 (defun select-mode--cleanup ()
@@ -158,7 +206,7 @@ on or off, this is set to the properties always or never.")
 (defun select-mode--undo-push ()
   "Add the current region to the undo list."
   (if (use-region-p)
-      (let ((el (list select-mode--type (region-beginning) (region-end))))
+      (let ((el (list select-mode--type select-mode--key (region-beginning) (region-end))))
         (unless (eq (last 'select-mode--undo-list) el)
           (push el select-mode--undo-list)))))
 
@@ -169,9 +217,10 @@ on or off, this is set to the properties always or never.")
   (when-let ((el (pop select-mode--undo-list))
              (type (car el)))
     (setq select-mode--type (car el))
-    (goto-char (nth 1 el))
-    (push-mark (point) t t)
+    (setq select-mode--key (nth 1 el))
     (goto-char (nth 2 el))
+    (push-mark (point) t t)
+    (goto-char (nth 3 el))
     (select-mode--update-numbers (select-mode--setting :next))))
 
 
@@ -241,20 +290,32 @@ This is safe to call it `select-mode' is not active."
     (select-mode--cleanup)))
 
 
-(defun select-mode--set-type (type)
+(defun select-mode--set-type (type &optional key)
   "Change to TYPE and update display.
 
 This is the common implementation of the keys that represent
 things like \"w\" for word.  If the current type is already TYPE
 then we expand the selection, which is the same as pressing 1."
-  (if (eq select-mode--type type)
-      (select-mode-expand)
+
     (let* ((settings (select-mode--settings type))
            (start (plist-get settings :start))
-           (next (plist-get settings :next)))
+           (next (plist-get settings :next))
+           (key2 (or key (plist-get settings :key))))
+
+
+      (if (and (eq select-mode--type type)
+               (eq select-mode--key  key2))
+          (select-mode-expand)
+
       (select-mode--undo-push)
 
       (setq select-mode--type type)
+      (setq select-mode--key key2)
+
+      (message "set-type %s key=%s key2=%s" type key key2)
+
+      (cl-assert start "Not a valid type?")
+      (cl-assert key2 "No key.  Did you forget to pass it for a pair?")
 
       ;; Return to the origin.
       (goto-char (overlay-start select-mode--origin))
@@ -322,6 +383,44 @@ then we expand the selection, which is the same as pressing 1."
   (when-let ((bounds (bounds-of-thing-at-point 'word)))
     (goto-char (cdr bounds))
     (point)))
+
+
+(defun select-mode--pair-start ()
+  "Select the pair around point."
+  (let ((bounds (select-mode--get-pair-bounds select-mode--key 'auto)))
+    (message "mark pair: %s" bounds)
+    (select-mode--mark-bounds bounds)))
+
+(defun select-mode--pair-next (num)
+  ;; There should be a region if select-mode--pair-start found an initial pair.
+  ;; If so, move to the beginning of it and go up.
+  ;;
+  ;; One thing to watch for: if the key is a right char, the region will be
+  ;; inside the pair and we need to backup 2 characters.
+
+  (when (use-region-p)
+    (let* ((pair (select-mode--make-pair select-mode--key))
+           (left (cdr pair)))
+      (goto-char (region-beginning))
+      (message "char-at 1: %s" (char-to-string (char-after)))
+      (if (/= (char-after) (string-to-char left))
+          (backward-char))
+      (message "char-at 2: %s" (char-to-string (char-after)))
+      (backward-char)
+      (let ((bounds (select-mode--get-pair-bounds select-mode--key 'auto)))
+        (message "pair next: %s %s" num bounds)
+        (select-mode--mark-bounds bounds)
+        bounds  ;; return bounds
+        )))
+  )
+
+
+(defun select-mode--mark-bounds (bounds)
+  "Internal function to set the active region to the cons cell BOUNDS."
+  (push-mark (point) t)
+  (goto-char (car bounds))
+  (push-mark (point) t t)
+  (goto-char (cdr bounds)))
 
 
 (defun select-mode--sexp-start ()
@@ -427,7 +526,9 @@ This implements numbers 1-9, which are handled by generated lambdas."
 
 This is used when a type does not supply its own :next function."
   (if-let ((pos (nth (1- num) select-mode--next)))
-    (goto-char pos)))
+      (if (listp pos)
+          (select-mode--mark-bounds pos)
+        (goto-char pos))))
 
 
 (defun select-mode-expand ()
@@ -454,17 +555,19 @@ This is used when a type does not supply its own :next function."
     (select-mode--cancel-timers)
     (setq select-mode--next nil)
 
-    ;; Call `nextfun' 9 times, or until it returns nil.
-    (save-excursion
-      (let ((count 0))
-        (while (and (< count 9)
-                    (progn
-                      (let ((pos (funcall nextfun (1+ count))))
-                        (when (not (null pos))
-                          (setq count (1+ count))
-                          (push pos select-mode--next)
-                          t ; return true to keep while loop going
-                          )))))))
+    ;; Call `nextfun' 9 times, or until it returns nil.  We also catch errors,
+    ;; to allow the :next implementations to be as simple as possible.
+    (save-mark-and-excursion
+      (ignore-errors
+          (let ((count 0))
+            (while (and (< count 9)
+                        (progn
+                          (let ((pos (funcall nextfun (1+ count))))
+                            (when (not (null pos))
+                              (setq count (1+ count))
+                              (push pos select-mode--next)
+                              t ; return true to keep while loop going
+                              ))))))))
 
     (setq select-mode--next (nreverse select-mode--next))
 
@@ -514,7 +617,15 @@ This is used when a type does not supply its own :next function."
   (let ((index 0))
     (setq select-mode--overlays
           (mapcar (lambda (pos)
+                    ;; `pos' can be an integer position or for pairs it can be a
+                    ;; (beg end) pair of positions.  If it is a pair, we'll put
+                    ;; the number at the beginning.
+                    (if (listp pos)
+                        (setq pos (car pos)))
+
+                    (message "pos=%s" pos)
                     (goto-char pos)
+
                     (let ((text (propertize (number-to-string (1+ index)) 'face 'select-mode-number))
                           (char (char-to-string (char-after))))
 
@@ -556,5 +667,149 @@ This is used when a type does not supply its own :next function."
 
 
 (provide 'select-mode)
+
+
+;; PAIRS
+;;
+;; I copied a lot of this from my surround package which I intend to replace
+;; with this.  I didn't want any dependencies.
+
+
+(defun select-mode--make-pair (char)
+  "Return a cons cell of (left . right) defined by CHAR.
+
+If CHAR is either a left or right character in `select-mode-pairs',
+that pair is returned.  Otherwise a cons cell is returned made of
+\(CHAR . CHAR)."
+  (or (assoc char select-mode-pairs)
+      (rassoc char select-mode-pairs)
+      (cons char char)))
+
+
+(defun select-mode--get-pair-bounds (char ends)
+  "Return bounds of text surrounded by CHAR, including CHAR.
+
+The ENDS parameter can be \\='outer to include the pairs, \\='inner
+to include the region within the pairs, or \\='auto
+which defaults to include the pairs unless CHAR is a
+right character in `select-mode-pairs'."
+  (let* ((pair (select-mode--make-pair char))
+         (left (car pair))
+         (right (cdr pair))
+         (bounds (select-mode--pair-bounds left right)))
+    (if (or (eq ends 'inner)
+            (and (eq ends 'auto)
+                 (string= char left)))
+        (select-mode--shrink bounds)
+      bounds)))
+
+
+(defun select-mode--pair-bounds (left right)
+  "Return the bounds of the enclosing pair LEFT and RIGHT."
+
+  ;; If the left and right are not the same, like ( and ), then we need a
+  ;; complex search that handles nesting.  For example if point is on "c" below,
+  ;; we need to select the entire expression and not just the closest parens to
+  ;; "c".
+  ;;
+  ;;     (a (b) c (d) e)
+  ;;
+  ;; Otherwise the open and close are the same and we won't support nesting.
+  ;; We'll use a simpler technique.
+  (if (string= left right)
+      (cons (search-forward left nil nil -1)
+            (search-forward right nil nil 1))
+    (cons
+     (select-mode--find-char-nestable left right -1)
+     (select-mode--find-char-nestable right left 1))))
+
+
+(defun select-mode--find-char-nestable (char other dir)
+  "Return the position of the closest, unnested CHAR.
+
+OTHER is the opposite pair character for CHAR.  For example if
+CHAR is ( then OTHER would be ).  DIR must be -1 to search
+backwards and 1 to search forward."
+  ;; This searches in a single direction (-1 or 1) for `char`.  When searching
+  ;; forward for the end of a pair of parens, we'd want to find the `char` ")".
+  ;; We need to skip nested pairs, so we watch for "(" which we call `other'.
+  ;;
+  ;; The only good algorithm I know is to walk forward to the next of either
+  ;; character and count the nesting level.  Since we are expecting to be
+  ;; inside, we start at 1.  When we reach 0, we've found the end of the current
+  ;; level.
+
+  (save-excursion
+    (if (looking-at (regexp-quote char))
+        ;; We're already on the character we're looking for.  If it is the
+        ;; opening paren (dir=-1), then this is the position we want.  If it is
+        ;; the closing paren (dir=1), then we want the *next* point since the
+        ;; selection is exclusive.
+        (when (= dir 1)
+          (forward-char 1))
+
+      ;; The algorithm below is going to assume we are *inside* a pair (level =
+      ;; 1), so if we're on an opening paren step in.  Otherwise the algorithm
+      ;; below will find the opening paren we're already on and count it as
+      ;; another open.
+      (if (and (looking-at (regexp-quote other)) (= dir 1))
+          (forward-char dir))
+
+      (let ((level 1))                      ; level of nesting
+        (while (> level 0)
+          (let* ((current (point))
+                 (charpos (search-forward char nil t dir))
+                 (otherpos (progn
+                             (goto-char current)
+                             (search-forward other nil t dir)))
+                 (chardist (select-mode--distance current charpos))
+                 (otherdist (select-mode--distance current otherpos))
+                 (pos (if (< chardist otherdist) charpos otherpos))
+                 (diff (if (< chardist otherdist) -1 1)))
+
+            (if (null charpos)
+                (user-error "Did not find %s" char))
+
+            (setq level (+ level diff))
+            (goto-char pos)))))
+
+    (point)))
+
+(defun select-mode--infer-bounds (&optional strict)
+  "Infer the bounds.
+
+With an active region they are those of the region. If looking at
+a symbol they are those of the symbol. For empty lines the
+current point is considered both start and end.
+
+If STRICT is t, a user error is signaled when the bounds couldn't
+be inferred."
+  (cond
+   ((use-region-p)
+    (cons (region-beginning) (region-end)))
+   ((thing-at-point 'symbol)
+    (bounds-of-thing-at-point 'symbol))
+   ((save-excursion
+      (beginning-of-line)
+      (looking-at-p "[ \t]*$"))
+    (cons (point) (point)))
+   (strict
+    (user-error "No bounds can be inferred"))))
+
+(defun select-mode--shrink (bounds)
+  "Shrink BOUNDS from outer to inner."
+  (cons (1+ (car bounds)) (1- (cdr bounds))))
+
+
+(defun select-mode--distance (p1 p2)
+  "Return the absolute distance between the positions P1 to P2."
+  ;; REVIEW: Aren't p1 and p2 positions which can't be negative?
+  ;;
+  ;; Also, using most-positive-fixnum does simplify other calling code, but
+  ;; perhaps we can reformat the other code to not require this.
+  (let* ((a1 (abs p1))
+         (a2 (abs (or p2 most-positive-fixnum))))
+    (- (max a1 a2) (min a1 a2))))
+
 
 ;;; select-mode.el ends here
